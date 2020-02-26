08a9755e9ae1c3af9137175cc214301ce0bda066
Gevorg98 <sarkis.petrosyan1993@gmail.com>
2020-02-26 12:59:07 +0400

    Was added git ignore

--- db/migrate/20200216212755_add_coulms__to_customers.rb
+++
@@ -1,7 +0,0 @@
-class AddCoulmsToCustomers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :customers, :email, :string
-    add_column :customers, :login, :string
-    add_column :customers, :password, :string
-  end
-end
